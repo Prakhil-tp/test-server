26afb78f29ecfe0b9388c1b0a7f1bafc63ca146b
prakhil-tp <prakhil.tp@gmail.com>
2020-11-12 15:15:30 +0530

    feat: Read string from input file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 const sha512 = require("js-sha512");
 
 http
@@ -6,7 +7,8 @@ http
     res.writeHead(200, { "Content-Type": "text/plain" });
 
     if (req.url === "/digest") {
-      const digest = sha512(Date.now().toString());
+      const data = fs.readFileSync("./input.txt");
+      const digest = sha512(data);
       res.write(`digest: ${digest} `);
     } else {
       res.write(`test-server: ${Date.now()} `);
